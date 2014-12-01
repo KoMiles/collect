e214345f45234ed299e3031de72ce35788c4de01
wangkongming <komiles@163.com>
2014-12-01 23:22:14 +0800

    *add wedding files

---
+++ wedding/index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="renderer" content="webkit">
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+<title>王孔明 & 章冰结婚请柬</title>
+<meta name="keywords" content="电子请柬,请柬">
+<meta name="description" content="王孔明 & 章冰结婚请柬">
+</head>
+<!-- 1 -->
+<frameset rows="0,*" frameborder="0" framespacing="0">
+  <frame  name="tops" id="tops" src="top.html" noresize="noresize">
+  <frame id="main" name="main" src="welcome.html#<?php echo $_SERVER['QUERY_STRING'];?>" noresize="noresize">
+</frameset>
+</html>
