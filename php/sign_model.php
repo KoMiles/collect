dd4240be8033bddb672a22bf32ecda65b7b40051
komiles <komiles@163.com>
2015-04-27 18:48:00 +0800

    *添加书签和设计模式网址

---
+++ php/sign_model.php
@@ -0,0 +1,18 @@
+/*=============================================================================
+#     FileName: sign_model.php
+#         Desc: php设计模式学习地址
+#       Author: wangkongming
+#        Email: komiles@163.com
+#     HomePage: http://www.wangkongming.cn/
+#      Version: 0.0.1
+#   LastChange: 2015-04-27 18:44:22
+#      History:
+=============================================================================*/
+<?php
+http://www.cnblogs.com/marixh/p/4283151.html
+http://www.cnblogs.com/siqi/archive/2012/09/09/2667562.html
+http://www.cnblogs.com/cocowool/archive/2009/03/18/1415660.html
+http://www.cnblogs.com/datang/archive/2011/03/21/1990532.html
+http://www.cnblogs.com/aqsmoke/p/3955310.html
+http://www.cnblogs.com/linuxnotes/archive/2013/05/28/3102907.html
+http://www.cnblogs.com/luojianqun/p/4115466.html
