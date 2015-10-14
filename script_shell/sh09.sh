c31d0e36cf0614f7be20092f473b78d59a0f7c8a
komiles <komiles@163.com>
2015-10-14 10:47:01 +0800

    *add shell file

---
+++ script_shell/sh09.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##
+##循环10次
+##
+for ((i=0; i<10; i++));
+
+do
+
+    echo Good Morning ,this is  $i  shell program.
+
+done
+echo ===============================
+for i in `seq 10`
+do
+
+    echo Good Morning ,this is  $i  shell program.
+
+done
---
+++ script_shell/sh10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                        
+##                                 
+##循环10次                         
+##                                 
+for ((i=0; i<10; i++));            
+
+do                                 
+
+    echo Good Morning ,this is  $i  shell program.                                                                        
+
+done 
