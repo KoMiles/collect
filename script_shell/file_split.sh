535d6b0b3ca27a90dbb8b52aaca18b4c615b2fe5
komiles <komiles@163.com>
2015-10-22 19:14:22 +0800

    *update

--- script_shell/file_split.sh
+++ script_shell/file_split.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+#文件作用：平均分隔大文件
 #执行方式：sh file_split.sh 26.txt 3
-#10个以内的文件
-#获取文件总的行数
+#执行结果：26_00.txt　26_01.txt 26_02.txt
+
 total_num=$(cat $1 | wc -l)
-#获取每个文件的行数
 split_num=$(($total_num/$2))
+
+#校验参数
+if [ $total_num -lt $2 ]
+then
+    echo "param  num $2 is to large"
+    exit
+fi
 
 #获取文件的名字
 file_name_tmp=$1
 file_name=${file_name_tmp%%.txt*}
-`split -l $split_num $1 -d $file_name"_tmp"`
 
-#分隔后总的文件数
-total_file_num=`ls ${file_name}_* | wc -l`
-for i in `seq 1 $2`
+`split -l $split_num $1 -d $file_name"_"`
+#重命名
+for files in `ls ${file_name}_*`
 do
-    mv ${file_name}_tmp0$(($i-1)) ${file_name}_0${i}.txt
+    mv $files $files.txt
 done
